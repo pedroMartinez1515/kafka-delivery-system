3c1403321003ca113d4ea2528170a78a1ae90d16
pedrothemage <mulinhazikoli@gmail.com>
2022-06-03 14:13:42 -0300

    consumer app updated

--- consumer-app/app.js
+++ consumer-app/app.js
@@ -3,29 +3,33 @@ const express = require('express');
 const app = express();
 app.use(express.json())
 
-
-
-
 const kafka = new Kafka({
     clientId: 'my-app',
     brokers: ['kafka:9092'],
 });
 
-const consumer = kafka.consumer()
+const consumer = kafka.consumer({ groupId: 'my-app' })
 
-await consumer.connect()
-await consumer.subscribe({ topic: 'restaurant' })
+async function run(){
 
-await consumer.run({
-  eachMessage: async ({ topic, partition, message }) => {
-    console.log({
-      value: message.value.toString(),
+    await consumer.connect()
+    await consumer.subscribe({ topic: 'restaurant' })
+
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            console.log({
+                value: message.value.toString(),
+            })
+        },
     })
-  },
-})
+}
+
+run();
 
 
-app.listen(3001, () => {
-    console.log('Producer server has started');
-});
+
+
+// app.listen(3001, () => {
+//     console.log('Consumer server has started');
+// });
 
